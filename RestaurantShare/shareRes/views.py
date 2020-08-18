32ca364da527aff063456a5843ef2dacdd281602
newpullin <newpullin@naver.com>
2020-08-18 12:34:58 +0900

    setting urls and import template html

--- RestaurantShare/shareRes/views.py
+++ RestaurantShare/shareRes/views.py
@@ -1,3 +1,20 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'shareRes/index.html')
+
+
+def restaurantCreate(request):
+    return render(request, 'shareRes/restaurantCreate.html')
+
+
+def categoryCreate(request):
+    return render(request, 'shareRes/categoryCreate.html')
+
+
+def restaurantDetail(request):
+    return render(request, 'shareRes/restaurantDetail.html')
+

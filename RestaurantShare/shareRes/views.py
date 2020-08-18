69354c2327e75dce6ae10df7cbb469a7ed6cae60
newpullin <newpullin@naver.com>
2020-08-18 15:28:54 +0900

    create& read function for restaurant

--- RestaurantShare/shareRes/views.py
+++ RestaurantShare/shareRes/views.py
@@ -6,8 +6,8 @@ from .models import *
 
 def index(request):
     categories = Category.objects.all()
-    print(categories)
-    content = {'categories': categories}
+    restaurants = Restaurant.objects.all()
+    content = {'categories': categories, 'restaurants': restaurants}
     return render(request, 'shareRes/index.html', content)
 
 def restaurantCreate(request):
@@ -27,11 +27,26 @@ def Create_category(request):
     new_category.save()
     return HttpResponseRedirect(reverse('index'))
 
-def restaurantDetail(request):
-    return render(request, 'shareRes/restaurantDetail.html')
+def restaurantDetail(request, res_id):
+    restaurant = Restaurant.objects.get(id = res_id)
+    content = {'restaurant': restaurant}
+    return render(request, 'shareRes/restaurantDetail.html', content)
 
 def Delete_category(request):
     category_id = request.POST['categoryId']
     delete_category = Category.objects.get(id = category_id)
     delete_category.delete()
     return HttpResponseRedirect(reverse('cateCreatePage'))
+
+
+def Create_restaurant(request):
+    category_id = request.POST['resCategory']
+    category = Category.objects.get(id=category_id)
+    name = request.POST['resTitle']
+    link = request.POST['resLink']
+    content = request.POST['resContent']
+    keyword = request.POST['resLoc']
+    new_res = Restaurant(category=category, restaurant_name = name, restaurant_link = link,
+                         restaurant_content=content, restaurant_keyword=keyword)
+    new_res.save()
+    return HttpResponseRedirect(reverse('index'))
